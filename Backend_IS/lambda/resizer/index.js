be057b3f3e6feecb478b786ce363fa22fc609ea2
Wuyoder <namnus1993@gmail.com>
2022-05-13 11:41:41 +0800

    lambda resizer

--- Backend_IS/lambda/resizer/index.js
+++ Backend_IS/lambda/resizer/index.js
@@ -1,21 +1,15 @@
-//const { v4: uuidv4 } = require('uuid');
 const aws = require('aws-sdk');
-const util = require('util');
-console.log('befor sharp require');
 const sharp = require('sharp');
-//const jimp = require('jimp');
 const s3 = new aws.S3({
   region: 'ap-northeast-1',
   accessKeyId: process.env.ACCESS_KEY_ID,
   secretAccessKey: process.env.SECRET_ACCESS_KEY,
   signatureVersion: 'v4',
 });
-
-exports.handler = async (event, context, callback) => {
+exports.handler = async (event) => {
   const srcBucket = event.Records[0].s3.bucket.name;
   const dstBucket = srcBucket;
   const srcKey = event.Records[0].s3.object.key.replace(/\+/g, ' ');
-
   const dstKey = 'resize/' + srcKey;
   console.log('get bucket', srcBucket);
   console.log('get srckey', srcKey);
@@ -24,13 +18,11 @@ exports.handler = async (event, context, callback) => {
     console.log('Could not determine the image type.');
     return 'Could not determine the image type.';
   }
-
   const imageType = typeMatch[1].toLowerCase();
   if (imageType != 'jpg' && imageType != 'png') {
     console.log(`Unsupported image type: ${imageType}`);
     return `Unsupported image type: ${imageType}`;
   }
-
   try {
     const params = {
       Bucket: srcBucket,
@@ -41,7 +33,6 @@ exports.handler = async (event, context, callback) => {
     console.log(err);
     return err;
   }
-
   console.log('before sharp resize');
   try {
     console.log('try resize');
@@ -65,7 +56,6 @@ exports.handler = async (event, context, callback) => {
     console.log(err);
     return err;
   }
-
   console.log(
     'Successfully resized ' +
       srcBucket +
